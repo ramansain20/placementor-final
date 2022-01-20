073a4b1ff0374a8bcf34e277a42332a45efe7ffe
ramansain20 <20je0766@ece.iitism.ac.in>
2022-01-21 00:44:01 +0530

    data.js improved and done

--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -1,79 +1,55 @@
 import { useEffect, useState } from "react";
-import { Accordion, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-
-
-
 const Data = () => {
-  const [arr,setArr]=useState([]);
   const params = useParams();
   console.log(params);
 
   const [data, setData] = useState({});
- 
-  const objectHandler=(obj)=>{
-    console.log("hello");
-    console.log(obj);
-     const values= Object.keys(obj).map((e,i)=>{
-       obj[e] && typeof obj[e]==='object'? objectHandler(obj[e]): setArr((prev)=>{
-        return [...prev,{key:e,value:obj[e]}]
-       }) 
-       
-     });
-     
-   }
 
-   useEffect(() => {
+  useEffect(() => {
     axios
       .get(`http://localhost:3000/${params.type}/${params.id}`)
       .then((response) => {
         console.log(response.data);
-        setData({data:response.data});
-       
-      }).then(()=>{
-        objectHandler(data)
+        setData(response.data);
       })
       .catch((e) => {
         console.log(e);
       });
   }, [params]);
 
-
-
   if (params.type !== "placement" && params.type !== "internship") {
     return <>Not found</>;
   }
- 
-
-
 
   return (
-    
     <>
-
-    <h2>company details by name...</h2>
-
       <h1 className=" text-center mt-3"> Selection Process</h1>
       <Container className="sm-6 md-6 lg-6 flex">
-        <Accordion defaultActiveKey="1">
-          {/* {Object.keys(data).map((e, i) => {
+        <>
+          <h1>{data.company_name}</h1>
+          <img src={data.logo} alt="logo" draggable={false} />
+          {Object.keys(data).map((e, i) => {
+            if (e === "logo" || e === "_id" || e === "__v") {
+              return <></>;
+            }
             return (
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>{e}</Accordion.Header>
-                
-                <Accordion.Body>{data[e]}</Accordion.Body>
-              </Accordion.Item>
+              <div>
+                <h1>{e}</h1>
+                {typeof data[e] === "object" ? (
+                  Object.keys(data[e]).map((x, idx) => {
+                    return <div>{data[e][x]}</div>;
+                  })
+                ) : (
+                  <div>{data[e]}</div>
+                )}
+              </div>
             );
-          })} */}
-          { () => {objectHandler(data)}}
-          {console.log(arr)}
-          {arr.map((e)=>{
-            <> <h1>e.key</h1>
-             <p>e.value</p></>
           })}
-        </Accordion>
+        </>
       </Container>
     </>
   );
